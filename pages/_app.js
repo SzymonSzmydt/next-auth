af623104c88bd7f20e56b9b9b31e4617a7004f63
SzymonSzmydt <szymonszmydt@gmail.com>
2023-02-07 15:32:55 +0100

    setting new project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Navbar from "./../components/navbar";
+import "../components/navbar.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
